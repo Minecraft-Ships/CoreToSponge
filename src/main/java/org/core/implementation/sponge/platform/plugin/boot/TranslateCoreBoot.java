88d744611fde5e7939d2471478db6a76350cf68e
mose <nekronmoseley@gmail.com>
2024-01-20 20:07:02 +0000

    should load now

--- src/main/java/org/core/implementation/sponge/platform/plugin/boot/TranslateCoreBoot.java
+++ src/main/java/org/core/implementation/sponge/platform/plugin/boot/TranslateCoreBoot.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Plugin("translate_core")
+@Plugin("ships")
 public class TranslateCoreBoot {
 
     private final CoreToSponge core;
